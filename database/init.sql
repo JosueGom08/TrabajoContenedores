83133b05b7c98a93a3cf3d420367f95e7f725f09
CesarAMM <csaradolfo139@gmail.com>
2026-04-11 13:03:49 -0600

    se agrega dokercompose y database

---
+++ database/init.sql
@@ -0,0 +1,23 @@
+create table usuario (
+    id          int AUTO_INCREMENT PRIMARY KEY,
+    user_name   varchar(10)
+    nombre      varchar(50)
+)
+
+create table rol (
+    rol             int AUTO_INCREMENT PRIMARY KEY,
+    descripcion     varchar(30)
+)
+
+create table log(
+    idLog       int AUTO_INCREMENT PRIMARY KEY,
+    idUser      int,
+    accion      char(1),
+    datosAnt    varchar(150),
+    datosDes    varchar(150)
+)
+
+create table usaurio_rol(
+    usuario int,
+    rol     int
+)
