45074ac094668ec2dd729b50f094a438e398f845
CesarAMM <csaradolfo139@gmail.com>
2026-04-21 21:12:09 -0600

    prueba

--- app_php/php/dashboard.php
+++ app_php/php/dashboard.php
@@ -47,6 +47,8 @@ if (!isset($_SESSION['user_id'])) {
     <a href="logout.php" class="btn-logout">Cerrar Sesión</a>
 </div>
 
+
+
 </body>
     <script src="../js/list_usuarios.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
